f0ebc4f9ead6451a67c1b27be57d44853d776ed1
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-25 12:20:34 -0500

    Creaccion de la clase TV
    Se ha creado la clase TV con los atributos y se esta comprobando la funcionalidad del estado True o False para encendido y apagado.

---
+++ src/main/java/taller3/TV.java
@@ -0,0 +1,27 @@
+package taller3;
+
+public class TV {
+    private Marca marca;
+    private int canal;
+    private int precio;
+    private boolean estado;
+    private int volumen;
+    private Control control;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        this.canal = 1;
+        this. volumen = 1;
+        this.precio = 500;
+    }
+
+    public String estadoToString() {
+        if (estado) {
+            return "Encendido";
+        } else {
+            return "Apagado";
+        }
+    }
+
+}
