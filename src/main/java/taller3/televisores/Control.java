1f9ef51908ca512e3e737b0b1f8ab044d1acfa89
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-25 15:24:30 -0500

    Métodos turnOn y turnOff.
    Se han creado los metodos turnOn y turnOff los cuales cambiaran el estado del televisor de encendido a apagado.

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,17 @@
 package taller3.televisores;
 
 public class Control {
-    
+    private TV tv;
+
+    public Control(TV tv) {
+        this.tv = tv;
+    }
+
+    public void turnOn() {
+        tv.setEstado(true);
+    }
+
+    public void turnOff() {
+        tv.setEstado(false);
+    }
 }
