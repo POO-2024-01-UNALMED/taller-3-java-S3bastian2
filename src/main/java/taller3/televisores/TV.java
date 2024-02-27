e63531e33140c4a9b8b2f85f474681ee753057a1
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-02-26 19:40:56 -0500

    Cambio en el método canal.
    Se ha actualizado el método canal de la clase TV.

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -35,7 +35,11 @@ public class TV {
     }
 
     public void setCanal(int canal) {
-        this.canal = canal;
+        if (this.estado) {
+            if (canal >= 1 && canal <= 120) {
+                this.canal = canal;
+            }
+        }
     }
 
     public int getCanal() {
